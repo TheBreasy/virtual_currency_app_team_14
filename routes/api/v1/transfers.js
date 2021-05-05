b044b834dbe700d33b17124c009ed59d9f0eed77
TheBreasy <b.cautaerts@hotmail.be>
2021-05-05 16:08:47 +0200

    intial setup nodejs project with express js

---
+++ routes/api/v1/transfers.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        "status": "success",
+        "data": {
+            "todos": []
+        }
+    });
+});
+
+router.post("/", (req, res) => {
+    res.json({
+        "status": "success",
+        "data": {
+            "todo": {
+                "text": "learn Node.js"
+            }
+        }
+    });
+});
+
+module.exports = router;
