d96510328eb8b7a310fb0c006e6cf9471b336b98
TheBreasy <b.cautaerts@hotmail.be>
2021-05-22 23:14:07 +0200

    added login functionality to application using passport.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,9 @@ const signup = async (req, res, next) => {
     let username = req.body.username; // UI of postman
     let password = req.body.password;
 
-    const user = new User({username: username});
+    const user = new User({
+        username: username
+    });
     await user.setPassword(password);
     await user.save().then(result => {
         res.json({
@@ -17,4 +19,21 @@ const signup = async (req, res, next) => {
     });
 };
 
+const login = async (req, res, next) => {
+    const user = await User.authenticate()(req.body.username, req.body.password).then(result => {
+        res.json({
+            "status": "success",
+            "data": {
+                "user": result
+            }
+        })
+    }).catch(error => {
+        res.json({
+            "status": "error",
+            "message": error
+        })
+    });
+};
+
 module.exports.signup = signup;
+module.exports.login = login;
